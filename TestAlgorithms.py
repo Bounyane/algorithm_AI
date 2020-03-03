887c5a55ad58ea0e36cefe6c5647e886a6674076
Bounyane <30981088+Bounyane@users.noreply.github.com>
2020-03-03 23:30:12 +0100

    Add files via upload

---
+++ TestAlgorithms.py
@@ -0,0 +1,11 @@
+import  numpy as np
+import ANN
+
+
+model=ANN.ANN()
+model.input(dim=10)
+model.layer(dim=10,activation="tanh")
+model.layer(dim=23,activation="tanh")
+model.alyer(dim=20,actiavtion="leaky_relu")
+model.output(dim=4)
+model.fit(input=None,output=None,labs=0.2,learning_rate=0.08,iteration=500,show_loss=True)
